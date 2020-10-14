6cccb179823b7abfc3c1d7b9dc40f9831625cf37
RichardDerkman <45083599+RichardDerkman@users.noreply.github.com>
2020-10-14 17:39:21 -0400

    the new thing

---
+++ Last Diving Wish/Assets/script/moving.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    public int speed;
+ 
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+   float v = Input.GetAxisRaw("Vertical");
+
+   
+   gameObject.transform.position = new Vector3 (transform.position.x + (h * speed), 
+      transform.position.y + (v * speed));
+    }
+}
