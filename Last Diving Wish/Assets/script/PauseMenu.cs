ace01b8a53ba6af385c00928c99eef1f4dc8a2b3
JacobLowe1 <60052817+JacobLowe1@users.noreply.github.com>
2020-10-18 02:05:17 -0700

    Main Menu

---
+++ Last Diving Wish/Assets/script/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("fire2"))
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+            }
+
+            else
+            {
+                pauseMenu.SetActive(false);
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;
+    }
+}
