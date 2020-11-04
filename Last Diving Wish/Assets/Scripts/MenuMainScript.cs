f650ec16aa11d16ec405700e5eebfa17e411d955
FlyingWestie <55253450+FlyingWestie@users.noreply.github.com>
2020-11-04 16:38:17 -0500

    More Audio
    Added sound effect for pressing start button and background music to the true main menu

--- Last Diving Wish/Assets/Scripts/MenuMainScript.cs
+++ Last Diving Wish/Assets/Scripts/MenuMainScript.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuMainScript : MonoBehaviour
 {
+
+    public AudioClip airplane_pa;
+    public AudioSource soundSource;
+
+    public static object LocalizationManager { get; private set; }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        soundSource.Play();
+        StartCoroutine(WaitAndLoadScene(soundSource.clip.length));
     }
+        private IEnumerator WaitAndLoadScene(float length)
+        {
+            float duration = soundSource.clip.length;
+            soundSource.PlayOneShot(soundSource.clip);
+     
+            AsyncOperation sceneLoading = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+            sceneLoading.allowSceneActivation = false;
+     
+            yield return new WaitForSeconds(duration);
+
+            while (sceneLoading.progress < 0.9f) yield return null;
+
+            sceneLoading.allowSceneActivation = true;
+        }
 
     public void QuitGame()
     {
