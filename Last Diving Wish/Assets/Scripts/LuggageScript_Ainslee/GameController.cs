22f5899581ffad52fff24c1cc25f30bc748ad030
cdralenko <ainsleeflowers@knights.ucf.edu>
2020-11-04 04:44:03 -0500

    Luggage Spawner
    - added random spawner, random rotator, and luggage prefabs

---
+++ Last Diving Wish/Assets/Scripts/LuggageScript_Ainslee/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] luggageSprites;
+    public Vector3 spawnValues;
+    public int luggageCount;
+    public float spawnWait;
+    public float startWait;
+
+    private void Start()
+    {
+        StartCoroutine (SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        for (int i = 0; i < luggageCount; i++)
+        {
+            GameObject luggage = luggageSprites[Random.Range (0,luggageSprites.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(luggage, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
