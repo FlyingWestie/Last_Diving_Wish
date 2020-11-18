4febca3f63e041d04b3e6d6906938291924e46ec
JacobLowe1 <60052817+JacobLowe1@users.noreply.github.com>
2020-11-18 12:15:30 -0800

    Commit: messed with adding new doors for levels 1 and 2 as well as new moving scripts

---
+++ Last Diving Wish/Assets/Scripts/Moving2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Moving2 : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    float velX;
+    float velY;
+    Rigidbody rigBody;
+
+
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rigBody.velocity.y;
+        rigBody.velocity = new Vector2(velX * moveSpeed, velY);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Luggege"))
+        {
+            scoring.score1 += 1;
+            Destroy(GameObject.FindWithTag("Luggege"));
+            if (scoring.score1 >= 20)
+            {
+                SceneManager.LoadScene("Win1");
+            }
+        }
+
+    }
+}
